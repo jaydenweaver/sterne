9c15a49ec370ca0daade3a6f68e04b810d0f3b67
jaydenweaver <jayden_weaver@hotmail.com>
2025-08-03 21:26:51 +1000

    turns out i dont need wasm :o

--- wasm/engine.cpp
+++
@@ -1,5 +0,0 @@
-#include "engine.hpp"
-
-extern "C" float *get_positions(int canvas_width, int canvas_height) {
-  return nullptr;
-}
--- wasm/engine.hpp
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-extern "C" float *get_positions(int, int);
