27e44524e69fd8c9e6008cfe94669c499e9c21e4
jaydenweaver <jayden_weaver@hotmail.com>
2025-08-07 18:45:14 +1000

    text animation

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,7 +1,7 @@
 export default function Landing() {
   return (
     <div>
-      <div className='text-content'>
+      <div className='text-content fade-in'>
         <h1 style={{ marginBottom: '0px' }}>Hey, I'm Jayden.</h1>
       </div>
     </div>
