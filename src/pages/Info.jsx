ae7317905257a9c20efc14f78b91f029dbd2c48f
jaydenweaver <jayden_weaver@hotmail.com>
2025-08-11 13:15:07 +1000

    fade in view

--- src/pages/Info.jsx
+++ src/pages/Info.jsx
@@ -1,9 +1,62 @@
-
+import { useRef, useEffect } from 'react';
 export default function Info() {
+  const panelRef = useRef(null);
+
+  useEffect(() => {
+    if (panelRef.current) {
+      panelRef.current.scrollTop = 0;
+    }
+  }, []);
 
   return (
-    <div>
+    <div ref={panelRef} className='info-panel'>
       <h1>Info page</h1>
+      <p>a</p>
+      <p>a</p>
+      <p>a</p>
+      <p>a</p>
+      <p>a</p>
+      <p>a</p>
+      <p>a</p>
+      <p>a</p>
+      <p>a</p>
+      <p>a</p>
+      <p>a</p>
+      <p>a</p>
+      <p>a</p>
+      <p>a</p>
+      <p>a</p>
+      <p>a</p>
+      <p>a</p>
+      <p>a</p>
+      <p>a</p>
+      <p>a</p>
+      <p>a</p>
+      <p>a</p>
+      <p>a</p>
+      <p>a</p>
+      <p>a</p>
+      <p>a</p>
+      <p>a</p>
+      <p>a</p>
+      <p>a</p>
+      <p>a</p>
+      <p>a</p>
+      <p>a</p>
+      <p>a</p>
+      <p>a</p>
+      <p>a</p>
+      <p>a</p>
+      <p>a</p>
+      <p>a</p>
+      <p>a</p>
+      <p>a</p>
+      <p>a</p>
+      <p>a</p>
+      <p>a</p>
+      <p>a</p>
+      <p>a</p>
+      <p>a</p>
     </div> 
   );
 } 
